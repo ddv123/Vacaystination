f100f1c99268dff9424afdd586bdbe492eee5af8
David V <vudavid0@gmail.com>
2018-01-23 05:54:29 +0000

    update dburl

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
     
 // seedDB();  //seed the database
-// mongoose.connect('mongodb://localhost/yc10');
-mongoose.connect("mongodb://dave:alex@ds211558.mlab.com:11558/yelpcamp");
+
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://dave:alex@ds211558.mlab.com:11558/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
