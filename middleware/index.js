378fc2d9bcad959cb73af29d5e65cbcc254d9b49
David V <vudavid0@gmail.com>
2018-05-08 15:01:32 +0000

    fix admin permission on comments

--- middleware/index.js
+++ middleware/index.js
@@ -36,7 +36,7 @@ middlewareObj.checkCommentOwnership = function(req, res, next){
                 res.redirect("back");
             } else {
                 //does user own the cmapground
-                if(foundComment.author.id.equals(req.user._id)){
+                if(foundComment.author.id.equals(req.user._id) || (req.user.isAdmin)){
                     next();
                 } else {
                     req.flash("error", "You do not have permission to do that");
